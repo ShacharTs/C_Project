4b350771cd6ab0c7141be6b28cf0ad566b53db7f
ShacharTs <shachar.ts1@gmail.com>
2024-11-30 13:09:37 +0200

    update

--- Debug_code/debug_main.c
+++ Debug_code/debug_main.c
@@ -1,59 +1,51 @@
 #include <stdio.h>
-#include <math.h>
+#include "math.h"
 
-float square_root(int num)
-{
+float square_root(int num){
     float x = num;
     float y = 1;
     float epsilon = 0.000001;
-    while (x - y > epsilon)
-    {
+    while (x - y > epsilon){
         x = (x + y) / 2;
         y = num / x;
     }
     return x;
 }
 
-void bug1()
-{
-    int numbers[] = {4, 8, 15, 16, 23, 42};
+void bug1(){
+    const int numbers[] = {4, 8, 15, 16, 23, 42};
+    const size_t size = sizeof(numbers) / sizeof(numbers[0]);
     int sum = 0;
     double runningAvg = 0;
 
-    for (int i = 0; i <= 6; i++)
-    {
+    for (int i = 0; i < size; i++){
         sum = sum + numbers[i];
-        runningAvg = sum / 1 + i;
+        runningAvg = (float)sum / (1 + i);
         printf("After adding %d: sum = %d, average = %.2f\n",
                numbers[i], sum, runningAvg);
     }
 }
 
-void bug2()
-{
-    int squares[] = {1, 4, 9, 16, 25};
+void bug2(){
+    const int squares[] = {1, 4, 9, 16, 25};
     float results[5];
-    for (int i = 0; i < 5; i++)
-    {
+    size_t size = sizeof(squares) / sizeof(squares[0]);
+    for (int i = 0; i < size; i++){
         results[i] = square_root(squares[i]);
-        if (results[i] * results[i] != squares[i])
-
-        {
+        if (results[i] * results[i] != squares[i]){
             printf("Something went wrong with square root!\n");
         }
     }
 }
-void bug3()
-{
+
+void bug3(){
     unsigned int i = 10;
-    while (i >= 0)
-    {
+    while (i > 0){
         i--;
     }
 }
 
-int main()
-{
+int main(){
     bug1();
     bug2();
     bug3();
